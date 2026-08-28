a1a3781b37ed15398663daa285002142564639f3
NotOkaza <faizulanw4.r@gmail.com>
2026-08-28 10:32:56 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'mc-editor-v1';
+const CACHE_NAME = 'mc-editor-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(APP_SHELL))
-      .catch(() => {})
+      .catch(() => { })
   );
   self.skipWaiting();
 });
